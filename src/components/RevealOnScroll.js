e70d8a02f0f38673b2f5531f31ba9e145cbca22a
prabina pattnaik <prabinakpattnaik@gmail.com>
2026-02-12 18:12:14 +0530

    Push code for better UI/UX warning

--- src/components/RevealOnScroll.js
+++ src/components/RevealOnScroll.js
@@ -16,12 +16,13 @@ const RevealOnScroll = ({ children, threshold = 0.1, delay = 0 }) => {
             { threshold }
         );
 
-        if (ref.current) {
-            observer.observe(ref.current);
+        const currentRef = ref.current;
+        if (currentRef) {
+            observer.observe(currentRef);
         }
 
         return () => {
-            if (ref.current) observer.disconnect();
+            if (currentRef) observer.disconnect();
         };
     }, [threshold]);
 
